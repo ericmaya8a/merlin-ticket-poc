4e7f32ded0bcd294a7fe99a1e61faa4e98f87f71
pnglobant <pablo.najimovich@globant.com>
2025-01-10 22:21:33 -0300

    first draft of confirmation page

--- src/app/confirmation/page.tsx
+++ src/app/confirmation/page.tsx
@@ -35,7 +35,7 @@ export default function ConfirmationPage() {
         </section>
       </section>
       <section className="mx-auto flex w-[638px] flex-col items-center">
-        <Card className="mb-16 mt-8 w-[638px]">
+        <Card className="mb-32 mt-8 w-[638px]">
           <CardHeader className="align-middle">
             <CardTitle>
               <h2 className="text-center text-3xl">Your booking: 2349234234</h2>
@@ -43,59 +43,65 @@ export default function ConfirmationPage() {
           </CardHeader>
 
           <CardContent>
-            <div className="flex items-center justify-between px-2 py-2">
-              <span className="text-sm text-[#1E274A]">1 day ticket</span>{" "}
-            </div>
-            <div className="flex items-center justify-between px-2">
-              <div className="w-1/3 text-[#1E274A]">
-                <div className="flex items-center space-x-2">
-                  <Button
-                    variant="destructive"
-                    size="sm"
-                    // onClick={() =>
-                    //   handleGuestChange("children", "decrement")
-                    // }
-                  >
-                    -
-                  </Button>
-                  <span className="w-2 pr-4 text-center text-2xl font-bold">
-                    5
+            <div className="mx-auto w-[400px] space-y-2">
+              <div className="flex items-center justify-between py-2">
+                <span className="text-lg font-medium">1 day ticket</span>
+                <span className="text-lg font-medium text-primary">
+                  £160.00
+                </span>
+              </div>
+              <div className="my-4 border-t border-gray-200"></div>
+              <div className="flex items-center justify-between py-2">
+                <span className="text-lg font-medium">Reserve & Ride</span>
+                <span className="text-lg font-medium text-primary">
+                  £275.00
+                </span>
+              </div>
+              <div className="my-4 border-t border-gray-200"></div>
+              <div className="flex items-center justify-between py-2">
+                <span className="text-lg font-medium text-primary">
+                  1x Express parking
+                </span>
+                <span className="text-lg font-medium text-primary">£20.00</span>
+              </div>
+              <div className="my-4 border-t border-gray-200"></div>
+
+              <div className="w-full space-y-2">
+                <div className="flex items-center justify-between py-2">
+                  <span className="text-lg font-medium">Subtotal</span>
+                  <span className="text-lg font-medium text-primary">
+                    £455.00
                   </span>
-                  <Button variant="destructive" size="sm">
-                    +
-                  </Button>
+                </div>
+
+                <div className="flex items-center justify-between py-2">
+                  <span className="text-lg font-medium">VAT</span>
+                  <span className="text-lg font-medium text-primary">
+                    £91.00
+                  </span>
+                </div>
+
+                <div className="flex items-center justify-between py-2">
+                  <span className="text-2xl font-bold text-primary">Total</span>
+                  <span className="text-2xl font-bold text-primary">
+                    £455.00
+                  </span>
                 </div>
               </div>
-              <span className="w-2/3 text-end text-[#1E274A]">
-                <strong>£160.00</strong>
+            </div>
+
+            <div className="w-full border-t border-gray-200"></div>
+
+            <div className="flex items-center justify-center py-8">
+              <span className="text-2xl font-bold text-primary">
+                Saturday 14 Dec 2024
               </span>
             </div>
           </CardContent>
 
           <div className="my-4 border-t border-gray-200"></div>
 
-          <CardFooter>
-            <div className="w-full space-y-2">
-              <div className="flex items-center justify-between py-2">
-                <span className="text-sm font-medium">Subtotal</span>
-                <span className="text-sm font-semibold text-primary">
-                  £160.00
-                </span>
-              </div>
-
-              <div className="flex items-center justify-between py-2">
-                <span className="text-sm font-medium">VAT</span>
-                <span className="text-sm font-semibold text-primary">
-                  £32.00
-                </span>
-              </div>
-
-              <div className="flex items-center justify-between py-2">
-                <span className="text-md font-bold text-primary">Total</span>
-                <span className="text-md font-bold text-primary">£160.00</span>
-              </div>
-            </div>
-          </CardFooter>
+          <CardFooter>Footer</CardFooter>
         </Card>
       </section>
     </>
